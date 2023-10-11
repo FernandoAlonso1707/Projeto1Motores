5198ac89340931ea576f5628947cf8a08a451393
Fernando Alonso Alves Capistrano <20221170150056@ifrn.local>
2023-10-11 10:04:30 -0300

    Projeto1

--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -5,13 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-    public int velocidade = 100;
+    public int velocidade = 10;
+    public int forcaPulo =7;
+    public  bool noChao;
     private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log(message:"START");
         TryGetComponent(out rb);
+    }
+    private void OnCollisionEnter(Collision collision){
+        if(!noChao && collision.gameObject.tag == "Chão"){
+            noChao = true;
+        }
     }
 
     // Update is called once per frame
@@ -21,6 +28,10 @@ public class Player : MonoBehaviour
          float h = Input.GetAxis("Horizontal"); // -1 esquerda, 0 nada, 1 direita
          float v = Input.GetAxis("Vertical"); // -1 pra trás,0 pra nada, 1 pra frente
          rb.AddForce(new Vector3(h, 0, v)* velocidade* Time.deltaTime, ForceMode.Impulse);
+         if(Input.GetKeyDown(KeyCode.Space) && noChao){
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+            noChao = false;
+         }
          if(transform.position.y <= -10){
              SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
